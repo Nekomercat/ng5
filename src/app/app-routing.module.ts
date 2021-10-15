4f8a530d0374f7dd940f250f9778bfeff3feaa59
adsoftsito <adsoft@live.com.mx>
2021-10-15 11:18:38 -0500

    add routing #6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {
+   path: '',  // root .. / 
+   component: HomeComponent
+ },
+ {
+   path: 'about/:id',   //  ../about
+   component: AboutComponent
+ }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
